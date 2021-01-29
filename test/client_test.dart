a8e8f3659f26c3c03cd410da9e64f9f085a46e0f
phamhieu <phamhieu1998@gmail.com>
2021-01-29 11:58:48 +0800

    chore: add basic test

---
+++ test/client_test.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:gotrue_client/gotrue_client.dart';
+import 'package:gotrue_client/src/session.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const supabaseUrl = '';
+  const annonToken = '';
+  GoTrueClient client;
+
+  setUp(() {
+    client = GoTrueClient(url: '$supabaseUrl/auth/v1', headers: {
+      'Authorization': 'Bearer $annonToken',
+      'apikey': annonToken,
+    });
+  });
+
+  test('basic json parsing', () async {
+    const body =
+        '{"access_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjExODk1MzExLCJzdWIiOiI0Njg3YjkzNi02ZDE5LTRkNmUtOGIyYi1kYmU0N2I1ZjYzOWMiLCJlbWFpbCI6InRlc3Q5QGdtYWlsLmNvbSIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIn0sInVzZXJfbWV0YWRhdGEiOm51bGwsInJvbGUiOiJhdXRoZW50aWNhdGVkIn0.GyIokEvKGp0M8PYU8IiIpvzeTAXspoCtR5aj-jCnWys","token_type":"bearer","expires_in":3600,"refresh_token":"gnqAPZwZDj_XCYMF7U2Xtg","user":{"id":"4687b936-6d19-4d6e-8b2b-dbe47b5f639c","aud":"authenticated","role":"authenticated","email":"test9@gmail.com","confirmed_at":"2021-01-29T03:41:51.026791085Z","last_sign_in_at":"2021-01-29T03:41:51.032154484Z","app_metadata":{"provider":"email"},"user_metadata":null,"created_at":"2021-01-29T03:41:51.022787Z","updated_at":"2021-01-29T03:41:51.033826Z"}}';
+    final bodyJson = json.decode(body);
+    final session = Session.fromJson(bodyJson as Map<String, dynamic>);
+
+    expect(session, isNotNull);
+    expect(session.accessToken,
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjExODk1MzExLCJzdWIiOiI0Njg3YjkzNi02ZDE5LTRkNmUtOGIyYi1kYmU0N2I1ZjYzOWMiLCJlbWFpbCI6InRlc3Q5QGdtYWlsLmNvbSIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIn0sInVzZXJfbWV0YWRhdGEiOm51bGwsInJvbGUiOiJhdXRoZW50aWNhdGVkIn0.GyIokEvKGp0M8PYU8IiIpvzeTAXspoCtR5aj-jCnWys');
+  });
+}
