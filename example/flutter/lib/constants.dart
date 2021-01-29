1dc48945c98789c4c1306fc70956e2016e0ee8a9
phamhieu <phamhieu1998@gmail.com>
2021-01-29 18:04:49 +0800

    chore: remove demo values

--- example/flutter/lib/constants.dart
+++ example/flutter/lib/constants.dart
@@ -7,9 +7,8 @@ const String PASSWORDRECOVER_SCREEN = 'PASSWORDRECOVER_SCREEN';
 
 const PERSIST_SESSION_KEY = 'PERSIST_SESSION_KEY';
 
-const SUPABASE_URL = 'https://nmbtadhxevkgnywohlie.supabase.net';
-const SUPABASE_ANNON_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMTcxNjE0MiwiZXhwIjoxOTI3MjkyMTQyfQ.utotClF3wl5mRf4Hl9gC_HP3edyLX-3rs4RCnKrRuKQ';
+const SUPABASE_URL = '';
+const SUPABASE_ANNON_KEY = '';
 final gotrueClient = GoTrueClient(url: '$SUPABASE_URL/auth/v1', headers: {
   'Authorization': 'Bearer $SUPABASE_ANNON_KEY',
   'apikey': SUPABASE_ANNON_KEY,
