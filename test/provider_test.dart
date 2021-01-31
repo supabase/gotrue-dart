8c5513f95a5d2fffb402f355fcc89990afe2f49d
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-01-31 14:18:56 +0900

    Added tests for provider and subscriptions

---
+++ test/provider_test.dart
@@ -0,0 +1,27 @@
+import 'package:gotrue/gotrue.dart';
+import 'package:gotrue/src/constants.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const gotrueUrl = 'http://localhost:9999';
+  const annonToken = '';
+
+  GoTrueClient client;
+
+  setUp(() {
+    client ??= GoTrueClient(url: gotrueUrl, headers: {
+      'Authorization': 'Bearer $annonToken',
+      'apikey': annonToken,
+    });
+  });
+
+  test('signIn() with Provider', () async {
+    final res = await client.signIn(provider: Provider.google);
+    final error = res.error;
+    final url = res.url;
+    final provider = res.provider;
+    expect(error, isNull);
+    expect(url, '$gotrueUrl/authorize?provider=google');
+    expect(provider, 'google');
+  });
+}
