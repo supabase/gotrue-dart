0a71c4c6ef70c0792e37e19988f9d135d76ccaec
phamhieu <phamhieu1998@gmail.com>
2021-02-01 12:10:33 +0800

    fix: updateUser bug

--- lib/src/user_attributes.dart
+++ lib/src/user_attributes.dart
@@ -6,10 +6,12 @@ class UserAttributes {
 
   UserAttributes({this.email, this.password, this.emailChangeToken, this.data});
 
-  Map<String, dynamic> toJson() => {
-        'email': email,
-        'password': password,
-        'email_change_token': emailChangeToken,
-        'data': data,
-      };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> values = {};
+    if (email != null) values['email'] = email;
+    if (password != null) values['password'] = email;
+    if (emailChangeToken != null) values['email_change_token'] = email;
+    if (data != null) values['data'] = data;
+    return values;
+  }
 }
