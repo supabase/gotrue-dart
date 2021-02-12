d7eb4b84986ca69d2e20cc587ac1848eb72999f3
GOLDCODERS CORP <55337687+goldcoders@users.noreply.github.com>
2021-02-13 02:27:39 +0800

    Update gotrue.dart

--- lib/gotrue.dart
+++ lib/gotrue.dart
@@ -4,5 +4,5 @@ export 'src/gotrue_client.dart';
 export 'src/provider.dart';
 export 'src/session.dart';
 export 'src/user.dart';
-export 'src/user_attributes';
+export 'src/user_attributes.dart';
 
