25a2ccbf54e94727ebafda43df992fce00974128
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-07-02 07:32:22 +0900

    Added comments withinn AuthOptions for developers to be able to understand what redirectTo and scopes mean

--- lib/src/auth_options.dart
+++ lib/src/auth_options.dart
@@ -2,5 +2,11 @@ class AuthOptions {
   final String? redirectTo;
   final String? scopes;
 
-  AuthOptions({this.redirectTo, this.scopes});
+  AuthOptions({
+    /// A URL or mobile address to send the user to after they are confirmed.
+    this.redirectTo,
+
+    /// A space-separated list of scopes granted to the OAuth application.
+    this.scopes,
+  });
 }
