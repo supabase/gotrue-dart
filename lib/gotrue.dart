dff635aa3305ff0828f4e5f4d34de862b837bdd6
phamhieu <phamhieu1998@gmail.com>
2021-07-05 10:25:21 +0700

    fix: export gotrue_response classes

--- lib/gotrue.dart
+++ lib/gotrue.dart
@@ -3,6 +3,7 @@ library gotrue;
 export 'src/auth_options.dart';
 export 'src/constants.dart' show AuthChangeEvent;
 export 'src/gotrue_client.dart';
+export 'src/gotrue_response.dart';
 export 'src/provider.dart';
 export 'src/session.dart';
 export 'src/user.dart';
