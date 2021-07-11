896a7dca833444effd6553d4e13dcd22ed8090b3
Tyler <18113850+dshukertjr@users.noreply.github.com>
2021-07-11 20:38:01 +0900

    Formatted code

--- lib/src/provider.dart
+++ lib/src/provider.dart
@@ -1,4 +1,14 @@
-enum Provider { apple, azure, bitbucket, discord, facebook, github, gitlab, google, twitter }
+enum Provider {
+  apple,
+  azure,
+  bitbucket,
+  discord,
+  facebook,
+  github,
+  gitlab,
+  google,
+  twitter
+}
 
 extension ProviderName on Provider {
   String name() {
