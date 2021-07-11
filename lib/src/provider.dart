444a51c3248253102c9981e9b79348545e8ee889
HarryET <29015545+HarryET@users.noreply.github.com>
2021-07-11 10:43:22 +0100

    Update External OAuth Providers

--- lib/src/provider.dart
+++ lib/src/provider.dart
@@ -1,4 +1,4 @@
-enum Provider { azure, bitbucket, facebook, github, gitlab, google, discord }
+enum Provider { apple, azure, bitbucket, discord, facebook, github, gitlab, google, twitter }
 
 extension ProviderName on Provider {
   String name() {
