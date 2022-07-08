75429972c31f7d7d4f316d9bbef8a073db8a977a
Eung Jin Lee <oiojin831@gmail.com>
2022-07-08 11:54:05 +0900

    able to update phone number with auth.update method

--- lib/src/user_attributes.dart
+++ lib/src/user_attributes.dart
@@ -1,14 +1,16 @@
 class UserAttributes {
   String? email;
+  String? phone;
   String? password;
   String? emailChangeToken;
   dynamic data;
 
-  UserAttributes({this.email, this.password, this.emailChangeToken, this.data});
+  UserAttributes({this.email, this.phone, this.password, this.emailChangeToken, this.data});
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> values = {};
     if (email != null) values['email'] = email;
+    if (phone != null) values['phone'] = phone;
     if (password != null) values['password'] = password;
     if (emailChangeToken != null) {
       values['email_change_token'] = emailChangeToken;
