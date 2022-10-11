c5140c48e048ae86c5b8b103c84337df5a579212
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-10-11 19:58:16 +0900

    adds admin test

--- test/admin_test.dart
+++ test/admin_test.dart
@@ -9,10 +9,10 @@ void main() {
   load(); // Load env variables from .env file
 
   final gotrueUrl = env['GOTRUE_URL'] ?? 'http://localhost:9998';
-  final anonToken = env['GOTRUE_TOKEN'] ?? 'anonKey';
+  // final anonToken = env['GOTRUE_TOKEN'] ?? 'anonKey';
   final email = env['GOTRUE_USER_EMAIL'] ?? 'fake$timestamp@email.com';
-  final phone = env['GOTRUE_USER_PHONE'] ?? '+1 666-0000-0000';
-  final password = env['GOTRUE_USER_PASS'] ?? 'secret';
+  // final phone = env['GOTRUE_USER_PHONE'] ?? '+1 666-0000-0000';
+  // final password = env['GOTRUE_USER_PASS'] ?? 'secret';
 
   final serviceRoleToken = JWT(
     {
@@ -30,12 +30,20 @@ void main() {
       client = GoTrueClient(
         url: gotrueUrl,
         headers: {
-          'Authorization': 'Bearer $anonToken',
-          'apikey': anonToken,
+          'Authorization': 'Bearer $serviceRoleToken',
+          'apikey': serviceRoleToken,
         },
       );
     });
 
-    test('', () {});
+    test('User registration', () {
+      test(
+          'inviteUserByEmail() creates a new user with an invited_at timestamp',
+          () async {
+        final res = await client.admin.inviteUserByEmail(email);
+        expect(res.user, isNotNull);
+        expect(res.user?.invitedAt, isNotNull);
+      });
+    });
   });
 }
