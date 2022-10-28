4af60ee1aa959806ae5bd240e175040848e12d55
Leynier Gutiérrez González <leynier41@gmail.com>
2022-10-28 08:31:37 -0400

    fix: avoid positive lookbehind in snake case extension (#101)
    * fix: avoid positive lookbehind in snake case extension
    
    * test: add tests for ToSnakeCase extension
    
    * perf: remove empty case in snakeCase
    
    The name of the enum value never is empty
    
    * perf: use final instead of var is possible

--- lib/src/types/auth_response.dart
+++ lib/src/types/auth_response.dart
@@ -84,9 +84,19 @@ class GenerateLinkProperties {
 
 extension ToSnakeCase on Enum {
   String get snakeCase {
-    RegExp exp = RegExp(r'(?<=[a-z])[A-Z]');
-    return name
-        .replaceAllMapped(exp, (Match m) => ('_${m.group(0)}'))
-        .toLowerCase();
+    final a = 'a'.codeUnitAt(0), z = 'z'.codeUnitAt(0);
+    final A = 'A'.codeUnitAt(0), Z = 'Z'.codeUnitAt(0);
+    final result = StringBuffer()..write(name[0].toLowerCase());
+    for (var i = 1; i < name.length; i++) {
+      final char = name.codeUnitAt(i);
+      if (A <= char && char <= Z) {
+        final pChar = name.codeUnitAt(i - 1);
+        if (a <= pChar && pChar <= z) {
+          result.write('_');
+        }
+      }
+      result.write(name[i].toLowerCase());
+    }
+    return result.toString();
   }
 }
