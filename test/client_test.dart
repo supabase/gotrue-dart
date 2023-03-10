f2d3b45277410e9c71ef5a948d5453a6564a0124
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-03-10 18:01:57 +0900

    test calling recovery quickly

--- test/client_test.dart
+++ test/client_test.dart
@@ -329,5 +329,14 @@ void main() {
         expect((error as AuthException).statusCode, '420');
       }
     });
+
+    test('Repeatedly recover session', () async {
+      await client.signInWithPassword(password: password, email: email1);
+      for (int i = 0; i < 10; i++) {
+        final json = client.currentSession!.persistSessionString;
+        print(json);
+        await client.recoverSession(json);
+      }
+    });
   });
 }
