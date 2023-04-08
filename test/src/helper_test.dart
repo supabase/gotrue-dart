860c9fc43143eaf51b40cd3ba8a418ccbf7e5187
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-04-09 00:35:19 +0900

    add test case to check the format of pkce code challenge

---
+++ test/src/helper_test.dart
@@ -0,0 +1,13 @@
+import 'package:gotrue/src/helper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+      'PKCE code verifier only contains alphanumeric characters, hyphens, periods, underscores and tildes',
+      () {
+    final codeVerifier = generatePKCEVerifier();
+    final codeChallenge = generatePKCEChallenge(codeVerifier);
+    final regex = RegExp(r'^[A-Za-z0-9-~_]*$');
+    expect(regex.hasMatch(codeChallenge), true);
+  });
+}
